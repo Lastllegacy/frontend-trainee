733ef96a8edcebbc520a0b18796218bbaa4c8055
Lastllegacy <forrkko@gmail.com>
2022-06-02 18:51:01 +0700

    Changes read.md

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./Layout/Layout";
 import Favorites from "./pages/Favorites";
 import Main from "./pages/Main";
-import { FavoritesContextProvider } from "./store/favorites-context";
 
 function App() {
   return (
-    <FavoritesContextProvider>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/favorites" element={<Favorites />} />
-        </Routes>
-      </Layout>
-    </FavoritesContextProvider>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </Layout>
   );
 }
 
